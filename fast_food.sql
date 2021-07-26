9d461a03dfadd4a981713a4dc25db9ea6dffa35a
AjayKamalakannan <70727091+ajaythecoder21@users.noreply.github.com>
2021-07-25 22:03:27 -0400

    SQL file

---
+++ fast_food.sql
@@ -0,0 +1,13 @@
+DROP TABLE FastFood;
+
+CREATE TABLE FastFood(
+city CHAR(200),
+country CHAR(200),
+latitude FLOAT,
+longitude FLOAT,
+fastfood_name VARCHAR(255),
+total_fastfood_count INT,
+McDCountry BOOLEAN
+
+
+);
